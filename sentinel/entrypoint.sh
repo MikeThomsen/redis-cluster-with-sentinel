5ba0c82d8ee2db2c61fc5573578ba2be178ab338
Mike Thomsen <mikerthomsen@gmail.com>
2018-03-30 18:42:04 -0400

    Consolidated the entire Redis Sentinel setup into a single docker image (sentinel, master + slave in one)

--- sentinel/entrypoint.sh
+++ sentinel/entrypoint.sh
@@ -5,4 +5,9 @@ sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /etc/redis/sentinel.conf
 sed -i "s/\$SENTINEL_DOWN_AFTER/$SENTINEL_DOWN_AFTER/g" /etc/redis/sentinel.conf
 sed -i "s/\$SENTINEL_FAILOVER/$SENTINEL_FAILOVER/g" /etc/redis/sentinel.conf
 
+exec docker-entrypoint.sh redis-server --port 6479 &
+exec docker-entrypoint.sh redis-server --port 6579 --slaveof localhost 6479 &
+
+sleep 5
+
 exec docker-entrypoint.sh redis-server /etc/redis/sentinel.conf --sentinel
